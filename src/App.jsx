81f3e7bf918d0a9deb3296d1cc4846943d32eb5f
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-02 04:46:57 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import './App.css'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Дневник питания Taste of <span>Health</span></h1>
+          <div>
+            <button>Войти</button>
+            <button>Регистрация</button>
+          </div>
+        </div>
+        <img src='https://img.freepik.com/premium-photo/blue-black-background-with-words-word-it_889056-15254.jpg'></img>
+      </section>
+    </>
+  )
+}
+
+export default App
