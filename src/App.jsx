c84bd500cd3907c274ac18532078d2b0337ea4ca
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-04 03:26:07 +0300

    added main cover img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import './App.css';
-import Lavender from "./assets/lavender.png";
+import Lavender from "./assets/BgLav.png";
 import Logo from "./assets/Health-logo.png";
 
 function App() {
@@ -10,17 +10,35 @@ function App() {
     <>
       <section className='welcome'>
         <div className='welcome__content'>
-          <img className='welcome__logo' src={Logo}></img>
+          <header className='header'>
+            <img className='welcome__logo' src={Logo}></img>
+            <nav>
+              <ul className='header__ul'>
+                <li>
+                  <button>Заказать консультацию</button>
+                </li>
+                <li>
+                  <button>Связаться со специалистом</button>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <h1 className='welcome__headline'>Дневник питания Taste of <span className='welcome__headline-span'>Health</span></h1>
           <div className='welcome__content-btns'>
-            
-            <button className='welcome__content-btns-button'>&rarr;</button>
-            <p className='welcome__content-btns-p'>Войти</p>
+            <div>
+              <button className='welcome__content-btns-button'>&rarr;</button>
+              <p >Войти</p>
+            </div>
+            <p className='welcome__content-btns-p'>Вы меняете <span className='welcome__headline-span-black'>привычки питания и стиль жизни</span> к лучшему, чтобы повысить качество жизни и эффективнее достигать поставленных целей</p>
             {/* <button>Регистрация</button> */}
           </div>
         </div>
         {/* <img className='welcome__img' src='https://img.freepik.com/premium-photo/blue-black-background-with-words-word-it_889056-15254.jpg'></img> */}
         <img className='welcome__img' src={Lavender}></img>
+        {/* <footer>
+          <div></div>
+          <ul></ul>
+        </footer> */}
       </section>
     </>
   )
