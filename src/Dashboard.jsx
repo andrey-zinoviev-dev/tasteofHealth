9002cc20f65cecf2ec8194b7e42b7c51d366863d
Andrey <sttrog_810@mail.ru>
2023-12-23 02:36:35 -0800

    updated dashboard component

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,29 +10,35 @@ export default function Dashboard() {
           <h2>Панель контроля питания</h2>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, iste pariatur et dolores iusto hic alias magnam nobis ratione illum, dignissimos quia quibusdam deleniti suscipit, dolore architecto laudantium officiis perferendis!</p>
         </div>
-        <div>
+        {/* <div>
           <p>Тут график веса будет</p>
-        </div>
-        {/* <img className="dashboard__head-wrapper-img" src={Diagram} /> */}
+        </div> */}
+        <img className="dashboard__head-wrapper-img" src={Diagram} />
       </div>
       <ul className="dashboard__ul">
         <li>
-          <div>
+          <div className="dashboard__ul-li-innerdiv">
             <span>Вода сегодня</span>
             <p><span>0.5L</span>/3L</p>
+            <div className="dashboard__ul-li-progress">
+              <div style={{width: `${Math.ceil(0.5/3*100)}%`}}></div>
+            </div>
           </div>
-          <div>
+          <div className="dashboard__ul-li-innerdiv" id="stats__calories">
             <span>Каллории в день</span>
             <p><span>1740</span>/2100 kcal</p>
+            <div className="dashboard__ul-li-progress">
+              <div style={{width: `${Math.ceil(1740/2100*100)}%`}}></div>
+            </div>
           </div>
-
         </li>
         <li id="stats__bento">
-          <div>
+          <div className="dashboard__ul-li-innerdiv">
             <span>Разы приема пищи</span>
             <p><span>2</span>раза</p>
           </div>
-          <div>
+          <div className="dashboard__ul-li-innerdiv">
+            <span>Питательные вещества за сутки</span>
               <p>Б в день- <span>30</span>гр</p>
               <p>Ж в день- <span>19</span>гр</p>
               <p>У в день- <span>5</span>гр</p>
